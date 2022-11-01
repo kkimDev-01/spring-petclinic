3fab961c1a544fc79485309d0d98770b77887933
kkimDev-01 <kkheentech@gmail.com>
2022-11-02 00:21:09 +0900

    auto-push

--- src/main/java/org/springframework/samples/petclinic/owner/SampleController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/SampleController.java
@@ -2,6 +2,7 @@ package org.springframework.samples.petclinic.owner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.samples.petclinic.aspect.LogExecutionTime;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,10 @@ public class SampleController {
 	@Autowired
 	ApplicationContext applicationContext;
 
+	@LogExecutionTime
 	@GetMapping("/context")
 	public String context() {
+
 		return "hello " + applicationContext.getBean(OwnerRepository.class);
 	}
 
